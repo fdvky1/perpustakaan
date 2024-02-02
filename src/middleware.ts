772cf3541df8184d98efaae96737f0e61158013f
Fdvky1 <fdvkey@gmail.com>
2024-02-02 17:05:58 +0700

    fix: uploadthing callback

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware"
 
 export const config = {
-    matcher: ['/((?!sign-in|sign-up|api/auth|_next/static|_next/image|favicon.ico).*)']
+    matcher: ['/((?!sign-in|sign-up|api/auth|api/uploadthing|_next/static|_next/image|favicon.ico).*)']
 }
