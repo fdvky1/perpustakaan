52f9eeb42eee7d0772b7a88712d13cc85203e6a9
Fdvky1 <fdvkey@gmail.com>
2024-02-03 17:50:56 +0700

    feat: crud category

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -4,10 +4,17 @@ import { FormEvent, useEffect, useState } from "react"
 import type { Category } from "@prisma/client";
 
 export default function CategoryPage(){
-    const [isOpen, setModal] = useState<boolean>(false);
+    const [modal, setModal] = useState<{action: "create" | "update" | "delete" | "", status: boolean, selected?: string, input?: string}>({
+        action: "",
+        status: false,
+        input: "",
+        selected: ""
+    });
+
     const [categories, setCategories] = useState<Category[]>([]);
-    const [input, setInput] = useState<string>("");
     const { setMessage } = useToastStore();
+
+    const resetModal = ()=> setModal({ action: "", status: false, input: "", selected: ""});
 
     const fetchCategory = () => fetch("/api/category").then(async res => {
         const json = await res.json();
@@ -20,45 +27,76 @@ export default function CategoryPage(){
 
     const create = async (e: FormEvent<HTMLFormElement>)=>{
         e.preventDefault();
-
         fetch('/api/category', {
             method: 'POST',
             body: JSON.stringify({
-                name: input
+                name: modal.input
             })
         }).then((res)=>{
             if (res.status === 200) {
                 fetchCategory();
-                setInput("");
-                setModal(false)
+                resetModal();
                 setMessage("Berhasil menambahkan kategori", "success");
             } else setMessage("Terjadi kesalahan, pastikan kategori belum ada!", "error");
         })
     }
 
+    const update = async (e: FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        fetch('/api/category/' + modal.selected, {
+            method: 'PUT',
+            body: JSON.stringify({
+                name: modal.input
+            })
+        }).then((res)=>{
+            if (res.status === 200) {
+                fetchCategory();
+                resetModal();
+                setMessage("Berhasil mengubah nama kategori", "success");
+            } else setMessage("Terjadi kesalahan saat mengubah kategori!", "error");
+        })
+    }
+
+    const deleteCategory = async (e: FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        fetch('/api/category/'+modal.selected, {
+            method: 'DELETE'
+        }).then((res)=>{
+            if (res.status === 200) {
+                fetchCategory();
+                resetModal();
+                setMessage("Kategori berhasil dihapus!", "success");
+            } else setMessage("Terjadi kesalahan saat menghapus kategori!", "error");
+        })
+    }
+
     return (
         <div className="container mx-auto pt-10 pb-16">
-            <input type="checkbox" id="my_modal_1" className="modal-toggle" checked={isOpen} onChange={(()=> setModal(!isOpen))} />
+            <input type="checkbox" id="my_modal_1" className="modal-toggle" checked={modal.status} readOnly />
             <div id="my_modal_1" className="modal" role="dialog">
                 <div className="modal-box">
-                    <h3 className="font-bold text-lg mb-3">Buat kategori baru</h3>
-                    <form className="w-full" onSubmit={create} id="create-form">
-                        <label className="form-control w-full">
-                            <div className="label">
-                                <span className="label-text mb-1">Nama kategori</span>
-                            </div>
-                            <input type="text" name="name" value={input} onChange={((e: FormEvent<HTMLInputElement>) => setInput(e.currentTarget.value))} className="input input-bordered w-full" />
-                        </label>
+                    <h3 className="font-bold text-lg mb-3">{modal.action == "create" ? "Buat kategori baru" : modal.action == "delete" ? "Hapus kategori" : modal.action == "update" ? "Perbarui kategori" : ""}</h3>
+                    <form className="w-full" onSubmit={modal.action == "delete" ? deleteCategory : modal.action == "update" ? update : create} id="create-form">
+                        {modal.action == "delete" ? (
+                            <p>Kategori akan dihapus secara permanen</p>
+                        ):(
+                            <label className="form-control w-full">
+                                <div className="label">
+                                    <span className="label-text mb-1">Nama kategori</span>
+                                </div>
+                                <input type="text" name="name" value={modal.input} onChange={((e: FormEvent<HTMLInputElement>) => setModal({...modal, input: e.currentTarget.value}))} className="input input-bordered w-full" />
+                            </label>
+                        )}
                     </form>
                     <div className="modal-action">
-                        <label htmlFor="my_modal_1" className="btn">Batalkan</label>
-                        <button className="btn btn-primary" type="submit" form="create-form">Simpan</button>
+                        <button className="btn" onClick={resetModal}>Batalkan</button>
+                        <button className={"btn "+ (modal.action == "delete" ? "btn-error" : "btn-primary")} type="submit" form="create-form">{modal.action == "delete" ? "Hapus" : "Simpan"}</button>
                     </div>
                 </div>
             </div>
             <div className="space-y-2">
                 <div className="w-full flex justify-end">
-                    <label htmlFor="my_modal_1" className="btn">Tambahkan kategori</label>
+                    <button onClick={(()=>setModal({ action: "create", status: true, input: "", selected: ""}))} className="btn">Tambahkan kategori</button>
                 </div>
                 <div className="overflow-x-auto">
                     <table className="table">
@@ -74,9 +112,12 @@ export default function CategoryPage(){
                                 <tr className="bg-base-200" key={i}>
                                     <th>{i+1}</th>
                                     <td>{c.name}</td>
-                                    <td>
-                                        <button className="btn btn-error py-0.5">
+                                    <td className="flex gap-1">
+                                        <button className="btn btn-error py-0.5" onClick={(()=> setModal({ action: "delete", status: true, selected: c.id, input: "" }))}>
                                             <i className="ri-delete-bin-7-line ri-xl text-white"></i>
+                                        </button>
+                                        <button className="btn btn-primary py-0.5" onClick={(()=> setModal({ action: "update", status: true, selected: c.id, input: c.name }))}>
+                                            <i className="ri-pencil-line ri-xl text-white"></i>
                                         </button>
                                     </td>
                                 </tr> 
