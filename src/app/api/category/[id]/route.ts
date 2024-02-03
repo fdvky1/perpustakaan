9dfb3d38cbbfc838e6e8d4cdacd83a2e2502a1f3
Fdvky1 <fdvkey@gmail.com>
2024-02-03 12:50:42 +0700

    feat: api update category

--- src/app/api/category/[id]/route.ts
+++ src/app/api/category/[id]/route.ts
@@ -7,11 +7,36 @@ interface Params {
     }
 }
 
+interface Payload {
+    name: string;
+}
+
 export async function DELETE(request: Request, { params }: Params){
     try {
-        const response = await prisma.category.delete({ where: { id: params.id } });
+        const response = await prisma.category.delete({
+            where:{
+                id: params.id
+            } 
+        });
         return NextResponse.json({ message: "Success", detail: response })
     }catch(e){
         return NextResponse.json({ message: "Failed to delete category"}, { status: 503})
+    }
+}
+
+export async function PUT(request: Request, { params }: Params){
+    try {
+        const { name } = await request.json() as Payload;
+        const response = await prisma.category.update({
+            where: {
+                id: params.id
+            },
+            data: {
+                name
+            }
+        });
+        return NextResponse.json({ message: "success",  detail: response });
+    } catch(e){
+        return NextResponse.json({ message: "Failed to update category"}, { status: 503 })
     }
 }
