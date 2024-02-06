de4b9d8cca1c4b21ad011d0f2cc3a4dfdbee8559
Fdvky1 <fdvkey@gmail.com>
2024-02-06 20:19:57 +0700

    feat: logout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,28 @@
 "use client";
 
-import { useSession } from 'next-auth/react';
+import { useSession, signOut } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
 export default function Header(){
     const pathname = usePathname();
     const session = useSession();
+    // const [logoutModal, setLogoutModal] = useState<boolean>(false);
+
     return ["/sign-in", "/sign-up"].includes(pathname) ? (<></>) : (
         <>
+            {/* Put this part before </body> tag */}
+            <input type="checkbox" id="modal_logout" className="modal-toggle" readOnly/>
+            <div className="modal" role="dialog">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">Peringatan!</h3>
+                    <p className="py-4">Apakah Anda yakin akan keluar?</p>
+                    <div className="modal-action">
+                        <label htmlFor="modal_logout" className="btn">Batalkan</label>
+                        <button type="button" className="btn btn-error" onClick={(()=> signOut({redirect: true, callbackUrl: "/login"}))}>Keluar</button>
+                    </div>
+                </div>
+            </div>
             <div className="navbar bg-base-100">
                 <div className="flex-none">
                     <label htmlFor="my-drawer" className="btn btn-square btn-ghost">
@@ -18,11 +32,28 @@ export default function Header(){
                 <div className="flex-1">
                     <a className="btn btn-ghost text-xl">Perpustakaan</a>
                 </div>
-                {/* <div className="flex-none">
-                    <button className="btn btn-square btn-ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-                    </button>
-                </div> */}
+                <div className="flex-none">
+                    <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                            <div className="avatar placeholder">
+                                <div className="bg-neutral text-neutral-content rounded-full w-8">
+                                    <span className="text-xs">{session.data?.user.name?.split(" ").map(v => v.charAt(0)).join("").toUpperCase()}</span>
+                                </div>
+                            </div>
+                        </div>
+                        <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                            {/* <li>
+                                <Link href="/profile" className="justify-between">
+                                    Profile
+                                    <span className="badge">New</span>
+                                </Link>
+                            </li> */}
+                            <li>
+                                <label htmlFor="modal_logout">Keluar</label>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
             <div className="drawer">
                 <input id="my-drawer" type="checkbox" className="drawer-toggle" />
