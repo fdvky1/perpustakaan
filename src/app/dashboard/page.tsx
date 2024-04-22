b780d00e59c39b4ae424adb6aa5fc80584cda73e
Fdvky1 <fdvkey@gmail.com>
2024-04-22 12:07:49 +0700

    fix: search

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,14 @@ import { getAuthSession } from '@/lib/auth'
 import Book from '@/app/book/page';
 import AdminDashboard from '@/app/dashboard/admin';
 
-export default async function Dashboard(){
+export default async function Dashboard({
+    searchParams
+}: {
+    searchParams?: {
+        keyword?: string;
+        page?: string;
+    }
+}){
     const session = await getAuthSession();
-    return ["admin", "operator"].includes(session!.user.role) ? <AdminDashboard/> : <Book/>
+    return ["admin", "operator"].includes(session!.user.role) ? <AdminDashboard/> : <Book searchParams={{keyword: searchParams?.keyword, page: searchParams?.page}}/>
 }
